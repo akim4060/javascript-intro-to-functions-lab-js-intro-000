52a8c57fb65738048e204bfaefa9c84aff71a169
Alex <akim4060@gmail.com>
2021-01-14 13:18:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma (string) {
-  var cantUnswer = "I can't hear you!";
-  var yesUnswer = "YES INDEED!";
-  var lovUnswer = "I love you, too.";
+  var deaf = "I can't hear you!";
+  var yes = "YES INDEED!";
+  var love = "I love you, too.";
   if (string.toLowerCase(string) === string) {
     return cantUnswer;
   }
